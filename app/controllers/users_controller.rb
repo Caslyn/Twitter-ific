c84b529cc43dc517a99112ee48bdb126bd32b96a
Caslyn Cole <caslyn.cole@gmail.com>
2014-07-22 13:19:02 -0700

    Finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,31 @@
 class UsersController < ApplicationController
-  def new
+
+  def show
+  	@user = User.find(params[:id])
   end
+
+  def new
+  	@user = User.new
+  end
+
+  	def user_params
+		# require the params hash to have a :user attribute 
+		# permit the name, email, password & password confirmation
+		# attributes (but no others)
+		params.require(:user).permit(:name, :email, :password,
+									 :password_confirmation)
+	end
+
+  def create
+  	# user_params only to be used internally
+  	@user = User.new(user_params) # Not the final implementation!
+  	if @user.save
+  		flash[:success] = "Welcome to Caslyn's Sample App!"
+  		redirect_to @user
+  	else
+  		render 'new'
+  	end
+  end
+
+	private :user_params
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,12 @@
 class User < ActiveRecord::Base
 	# Downcasing the email address before it is saved to the database
-	before_save { self.email = email.downcase }
+	before_save { email.downcase! }
 
 	# Validating the presence of a name
 	# Same as validates(:name, {presence: true ...})
 	validates :name, presence: true, length: {maximum: 50}
 	
-	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(?:\.[a-z\d\-]+)*\.[a-z]+\z/i
 	validates :email, presence: true, format: { with: VALID_EMAIL_REGEX },
 					  uniqueness: { case_sensitive: false }
 
