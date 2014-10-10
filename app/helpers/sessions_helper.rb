f7cb4e16bd9fd64a7ba4dd563ec6501a34e180f0
Caslyn Cole <caslyn.cole@gmail.com>
2014-10-09 20:00:43 -0700

    tweaks to code for heroku functionality

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,10 +1,12 @@
 module SessionsHelper
 
 	def sign_in(user)
-		remember_token = User.new_remember_token
-		cookies.permanent[:remember_token] = remember_token
-		user.update_attribute(:remember_token, User.digest(remember_token))
-		self.current_user = user
+		if user
+			remember_token = User.new_remember_token
+			cookies.permanent[:remember_token] = remember_token
+			user.update_attribute(:remember_token, User.digest(remember_token))
+			self.current_user = user
+		end
 	end
 
 	def current_user=(user)
